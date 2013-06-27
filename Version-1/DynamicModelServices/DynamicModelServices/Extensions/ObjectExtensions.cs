24a9389b323ea57284a49f4862ca9a2f914ed88f
ironpython2001@gmail.com <ironpython2001@gmail.com@83349859-938d-51d4-ba03-627d294f63ce>
2013-06-27 04:46:39 +0000

    Intitial Version files

---
+++ Version-1/DynamicModelServices/DynamicModelServices/Extensions/ObjectExtensions.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace DynamicModelServices
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Turns the object into an ExpandoObject
+        /// </summary>
+        public static dynamic ToExpando(this object o)
+        {
+            var result = new ExpandoObject();
+            var d = result as IDictionary<string, object>; //work with the Expando as a Dictionary
+            if (o.GetType() == typeof(ExpandoObject)) return o; //shouldn't have to... but just in case
+            if (o.GetType() == typeof(NameValueCollection) || o.GetType().IsSubclassOf(typeof(NameValueCollection)))
+            {
+                var nv = (NameValueCollection)o;
+                nv.Cast<string>().Select(key => new KeyValuePair<string, object>(key, nv[key])).ToList().ForEach(i => d.Add(i));
+            }
+            else
+            {
+                var props = o.GetType().GetProperties();
+                foreach (var item in props)
+                {
+                    d.Add(item.Name, item.GetValue(o, null));
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Turns the object into a Dictionary
+        /// </summary>
+        public static IDictionary<string, object> ToDictionary(this object thingy)
+        {
+            return (IDictionary<string, object>)thingy.ToExpando();
+        }
+    }
+     
+}
